3a0ac1e1cdff9ab166254f0483e419e0743ee919
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-02 13:47:59 +0530

    Added cart icon

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,15 +1,29 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Badge } from '@mui/material';
+import { useMemo } from 'react';
 import { emptyFavorites } from '@/redux/slice/cart.slice';
 import { Box, Typography } from '@mui/material';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useDispatch } from 'react-redux';
+import { useAppSelector } from '@/hooks/redux';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
     const { status } = useSession();
     const dispatch = useDispatch();
+    const { push } = useRouter();
+    const cartData = useAppSelector((state) => state.cart.cartData);
+
+    const totalCartQuantity = useMemo(() => {
+        return Object.values(cartData).reduce(
+            (prev, current) => prev + current.quantity,
+            0
+        );
+    }, [cartData]);
 
     return (
-        <Box className="flex items-center p-4 shadow fixed top-0 w-screen z-50 bg-white">
+        <Box className="flex items-center px-6 py-4 shadow fixed top-0 w-screen z-50 bg-white">
             <Typography component="h1" variant="h6" className="cursor-pointer">
                 <Link href="/">XMart</Link>
             </Typography>
@@ -17,6 +31,23 @@ const Navbar = () => {
             <Box className="flex-1" />
 
             <Box className="flex gap-3">
+                <Badge
+                    badgeContent={totalCartQuantity}
+                    onClick={() => {
+                        push('/cart');
+                    }}
+                    sx={{
+                        cursor: 'pointer',
+                    }}
+                    className="cart-badge"
+                >
+                    <ShoppingCartIcon
+                        sx={{
+                            color: '#002884',
+                        }}
+                    />
+                </Badge>
+
                 <Link href="/">Home</Link>
 
                 {status === 'authenticated' ? (
