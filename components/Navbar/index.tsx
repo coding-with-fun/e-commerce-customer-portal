78ec8e0595b0aacbd55f0caea462af441c5d6d9b
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-02 14:09:15 +0530

    Changed Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,19 +1,39 @@
+import AppBar from '@mui/material/AppBar';
+import { useAppSelector } from '@/hooks/redux';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Badge } from '@mui/material';
-import { useMemo } from 'react';
-import { emptyFavorites } from '@/redux/slice/cart.slice';
-import { Box, Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import { useDispatch } from 'react-redux';
-import { useAppSelector } from '@/hooks/redux';
 import { useRouter } from 'next/router';
+import { MouseEvent, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import env from '@/utility/env';
+import _ from 'lodash';
+import Link from 'next/link';
 
 const Navbar = () => {
-    const { status } = useSession();
+    const { status, data: session } = useSession();
     const dispatch = useDispatch();
     const { push } = useRouter();
     const cartData = useAppSelector((state) => state.cart.cartData);
+
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+    // const [openSideBar, setOpenSideBar] = useState(false);
+
+    const handleOpenUserMenu = (event: MouseEvent<HTMLElement>): void => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = (): void => {
+        setAnchorElUser(null);
+    };
 
     const totalCartQuantity = useMemo(() => {
         return Object.values(cartData).reduce(
@@ -23,54 +43,181 @@ const Navbar = () => {
     }, [cartData]);
 
     return (
-        <Box className="flex items-center px-6 py-4 shadow fixed top-0 w-screen z-50 bg-white">
-            <Typography component="h1" variant="h6" className="cursor-pointer">
-                <Link href="/">XMart</Link>
-            </Typography>
-
-            <Box className="flex-1" />
-
-            <Box className="flex gap-3">
-                <Badge
-                    badgeContent={totalCartQuantity}
-                    onClick={() => {
-                        push('/cart');
-                    }}
-                    sx={{
-                        cursor: 'pointer',
-                    }}
-                    className="cart-badge"
-                >
-                    <ShoppingCartIcon
-                        sx={{
-                            color: '#002884',
-                        }}
-                    />
-                </Badge>
-
-                <Link href="/">Home</Link>
-
-                {status === 'authenticated' ? (
-                    <Link href="/profile">Profile</Link>
-                ) : null}
-
-                {status === 'unauthenticated' ? (
-                    <Link href="/auth/signin">Sign In</Link>
-                ) : null}
-
-                {status === 'authenticated' ? (
+        <AppBar position="fixed">
+            <Container
+                sx={{
+                    maxWidth: '100% !important',
+                }}
+            >
+                <Toolbar disableGutters>
                     <Typography
-                        className="cursor-pointer"
+                        variant="h6"
+                        noWrap
                         onClick={() => {
-                            signOut();
-                            dispatch(emptyFavorites());
+                            push('/');
+                        }}
+                        sx={{
+                            mr: 2,
+                            display: {
+                                xs: 'none',
+                                md: 'flex',
+                            },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            cursor: 'pointer',
                         }}
                     >
-                        Sign Out
+                        {env.appName}
                     </Typography>
-                ) : null}
-            </Box>
-        </Box>
+
+                    <Box
+                        sx={{
+                            mr: 2,
+                            display: {
+                                xs: 'flex',
+                                md: 'none',
+                            },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="p"
+                            onClick={() => {
+                                push('/');
+                            }}
+                            sx={{
+                                cursor: 'pointer',
+                                fontFamily: 'monospace',
+                            }}
+                        >
+                            {env.appName}
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: {
+                                xs: 'none',
+                                md: 'flex',
+                            },
+                        }}
+                    />
+
+                    <Box
+                        sx={{
+                            marginRight: 3.5,
+                        }}
+                    >
+                        <Badge
+                            badgeContent={totalCartQuantity}
+                            onClick={() => {
+                                push('/cart');
+                            }}
+                            className="cart-badge"
+                        >
+                            <ShoppingCartIcon
+                                sx={{
+                                    color: '#ffffff',
+                                    cursor: 'pointer',
+                                }}
+                            />
+                        </Badge>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            flexGrow: 0,
+                            display: 'flex',
+                            gap: '1rem',
+                            ...(status === 'authenticated' && {
+                                display: 'none',
+                            }),
+                        }}
+                    >
+                        <Typography>
+                            <Link href="/auth/signin">Sign In</Link>
+                        </Typography>
+
+                        <Typography>
+                            <Link href="/auth/signup">Sign Up</Link>
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            flexGrow: 0,
+                        }}
+                    >
+                        <Tooltip title="Open settings">
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{
+                                    p: 0,
+                                    ...(status === 'unauthenticated' && {
+                                        display: 'none',
+                                    }),
+                                }}
+                            >
+                                <Avatar
+                                    alt={session?.user.name ?? ''}
+                                    src={session?.user.image ?? ''}
+                                />
+                            </IconButton>
+                        </Tooltip>
+
+                        <Menu
+                            sx={{
+                                mt: '45px',
+                            }}
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            <MenuItem
+                                onClick={() => {
+                                    push('/profile');
+                                    handleCloseUserMenu();
+                                }}
+                            >
+                                <Typography textAlign="center">
+                                    Profile
+                                </Typography>
+                            </MenuItem>
+
+                            <MenuItem
+                                onClick={() => {
+                                    signOut();
+                                }}
+                            >
+                                <Typography textAlign="center">
+                                    Sign Out
+                                </Typography>
+                            </MenuItem>
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 };
 
--- utility/env.ts
+++ utility/env.ts
@@ -1,4 +1,5 @@
 const env = {
+    appName: process.env.NEXT_PUBLIC_APP_NAME as string,
     baseURL: process.env.NEXT_PUBLIC_BASE_URL as string,
 
     auth: {
